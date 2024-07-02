04ecd68801bc48f147439332ffe156a3c68d74b8
Anatoly Batov <play.hoffa@gmail.com>
2024-07-02 21:59:20 +0400

    commit

--- operators.php
+++ operators.php
@@ -4,3 +4,31 @@ $aab = 'Мир!';
 $aaa = "Привет, $aab}";
 
 echo $aaa;
+
+name: CI
+
+on:
+  push:
+    branches:
+      - main
+      - develop
+      - feature/*
+  pull_request:
+    branches:
+      - main
+      - develop
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+
+    steps:
+      - uses: actions/checkout@v2
+      - name: Set up Node.js
+        uses: actions/setup-node@v2
+        with:
+          node-version: '14'
+      - name: Install dependencies
+        run: npm install
+      - name: Run tests
+        run: npm test
